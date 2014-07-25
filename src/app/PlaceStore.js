5e9401e19d5f787e3b7d8ebb4f3e5efd1237fd72
Zoltan Juhasz <juzoli@gmail.com>
2014-07-25 16:28:34 +0200

    Creating the base of the storage storing the places

---
+++ src/app/PlaceStore.js
@@ -0,0 +1,13 @@
+items : [ {
+				name : 'Weehawken',
+				coordinate : {
+					lat : 40.770008,
+					lng : -74.0260252
+				}
+			}, {
+				coordinate : 'Gyöngyös',
+				coordinate : {
+					lat : 47.8084735,
+					lng : 19.95185
+				}
+			} ]
