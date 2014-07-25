d51c00e7e78aadb1a186f10ccd5c1de3ed330a6f
Zoltan Juhasz <juzoli@gmail.com>
2014-07-25 16:27:07 +0200

    New buttons jumping to specific locations

--- src/app/gmap.js
+++ src/app/gmap.js
@@ -1,43 +1,70 @@
-Ext.Loader.setConfig({enabled: true});
-Ext.Loader.setPath('Ext.ux', '../ux');
-Ext.require([
-    'Ext.window.*',
-    'Ext.ux.GMapPanel'
-]);
+Ext.onReady(function() {
 
-Ext.application({
-    name   : 'MyApp',
+	var w = Ext.create('Ext.Panel', {
+		renderTo : Ext.getBody(),
+		layout : 'border',
+		height : 500,
+		hideLabel : true,
+		items : [ {
+			region : 'center',
+			xtype : 'gmappanel',
+			center : {
+				geoCodeAddr : 'Budakeszi, 99, Erdo utca, Magyarország',
+				marker : {
+					title : 'Keszi'
+				}
+			},
+			markers : [ {
+				lat : 47.5109394,
+				lng : 18.9297127,
+				title : 'Buszmegálló',
+				listeners : {
+					click : function(e) {
+						Ext.Msg.alert('Ide erkezel');
+					}
+				}
+			}, {
+				lat : 47.513150,
+				lng : 18.932394,
+				title : 'Itt fordulsz le'
+			} ]
+		}, {
+			region : 'west',
+			title : 'Locations',
+			collapsible : true,
+			width : 200,
+			items : [ {
+				xtype : 'button',
+				text : 'Weehawken',
+				coordinate : {
+					lat : 40.770008,
+					lng : -74.0260252
+				},
+				handler : jumpTo
+			}, {
+				xtype : 'button',
+				text : 'Gyöngyös',
+				coordinate : {
+					lat : 47.8084735,
+					lng : 19.95185
+				},
+				handler : jumpTo
+			} ]
+		} ]
 
-    launch : function() {
+	});
 
-    	Ext.create('Ext.Panel', {
-    		  renderTo     : Ext.getBody(),
-             layout: 'fit',
-             height: 500,
-             hideLabel: true,
-             items: {
-                 xtype: 'gmappanel',
-                 center: {
-                     geoCodeAddr: 'Budakeszi, 99, Erdo utca, Magyarország',
-                     marker: {title: 'Keszi'}
-                 },
-                 markers: [{
-                     lat: 47.5109394,
-                     lng: 18.9297127,
-                     title: 'Buszmegálló',
-                     listeners: {
-                         click: function(e){
-                             Ext.Msg.alert('Ide erkezel');
-                         }
-                     }
-                 },{
-                     lat: 47.513150,
-                     lng: 18.932394,
-                     title: 'Itt fordulsz le'
-                 }]
-             }
-        
-        });
+	/**
+	 * Just example, do not write code like this! GMApPanel source code
+	 * http://docs.sencha.com/extjs/4.2.0/extjs-build/examples/ux/GMapPanel.js
+	 */
 
-    }
+	// get the map reference
+	var map = w.down('gmappanel');
+
+	function jumpTo(button) {
+		map.setCenter(button.coordinate);
+	}
+
+	w.show();
 });
